dd7c22a2c0c1b12222b3b36c5f451c206403c33f
杰苏 <1119204194@qq.com>
2021-06-05 09:56:09 +0800

    posix标准库函数Portable Operating System Interface

---
+++ posix.php
@@ -0,0 +1,40 @@
+<?php
+
+POSIX 函数
+posix_access — Determine accessibility of a file
+posix_ctermid — Get path name of controlling terminal
+posix_errno — 别名 posix_get_last_error
+posix_get_last_error — Retrieve the error number set by the last posix function that failed
+posix_getcwd — Pathname of current directory
+posix_getegid — Return the effective group ID of the current process
+posix_geteuid — Return the effective user ID of the current process
+posix_getgid — Return the real group ID of the current process
+posix_getgrgid — Return info about a group by group id
+posix_getgrnam — Return info about a group by name
+posix_getgroups — Return the group set of the current process
+posix_getlogin — Return login name
+posix_getpgid — Get process group id for job control
+posix_getpgrp — Return the current process group identifier
+posix_getpid — 返回当前进程 id
+posix_getppid — Return the parent process identifier
+posix_getpwnam — Return info about a user by username
+posix_getpwuid — Return info about a user by user id
+posix_getrlimit — Return info about system resource limits
+posix_getsid — Get the current sid of the process
+posix_getuid — Return the real user ID of the current process
+posix_initgroups — Calculate the group access list
+posix_isatty — Determine if a file descriptor is an interactive terminal
+posix_kill — Send a signal to a process
+posix_mkfifo — Create a fifo special file (a named pipe)
+posix_mknod — Create a special or ordinary file (POSIX.1)
+posix_setegid — Set the effective GID of the current process
+posix_seteuid — Set the effective UID of the current process
+posix_setgid — Set the GID of the current process
+posix_setpgid — Set process group id for job control
+posix_setrlimit — Set system resource limits
+posix_setsid — Make the current process a session leader
+posix_setuid — Set the UID of the current process
+posix_strerror — Retrieve the system error message associated with the given errno
+posix_times — Get process times
+posix_ttyname — Determine terminal device name
+posix_uname — Get system name
