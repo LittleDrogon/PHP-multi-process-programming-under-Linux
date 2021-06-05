910068c3d5e7e50b8f6e897d3700a9c357a17962
杰苏 <1119204194@qq.com>
2021-06-05 10:15:50 +0800

    shared Memory函数

---
+++ shareMemory.php
@@ -0,0 +1,10 @@
+<?php
+
+Shared Memory 函数
+shmop_close — Close shared memory block
+shmop_delete — Delete shared memory block
+shmop_open — Create or open shared memory block
+shmop_read — Read data from shared memory block
+shmop_size — Get size of shared memory block
+shmop_write — Write data into shared memory block
+Shmop — The Shmop class
