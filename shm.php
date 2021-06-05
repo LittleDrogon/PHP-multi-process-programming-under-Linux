bec6d8e76c0676d6201c415a11085abb9d9a1dd3
杰苏 <1119204194@qq.com>
2021-06-05 10:06:36 +0800

    semaphore函数

---
+++ shm.php
@@ -0,0 +1,22 @@
+<?php
+
+Semaphore 函数
+ftok — Convert a pathname and a project identifier to a System V IPC key
+msg_get_queue — Create or attach to a message queue
+msg_queue_exists — Check whether a message queue exists
+msg_receive — Receive a message from a message queue
+msg_remove_queue — Destroy a message queue
+msg_send — Send a message to a message queue
+msg_set_queue — Set information in the message queue data structure
+msg_stat_queue — Returns information from the message queue data structure
+sem_acquire — Acquire a semaphore
+sem_get — Get a semaphore id
+sem_release — Release a semaphore
+sem_remove — Remove a semaphore
+shm_attach — Creates or open a shared memory segment
+shm_detach — Disconnects from shared memory segment
+shm_get_var — Returns a variable from shared memory
+shm_has_var — Check whether a specific entry exists
+shm_put_var — Inserts or updates a variable in shared memory
+shm_remove_var — Removes a variable from shared memory
+shm_remove — Removes shared memory from Unix systems
